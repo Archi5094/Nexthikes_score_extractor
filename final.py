2377c19abb64af0662746c5606376e82af809d5d
Archi5094 <86299671+Archi5094@users.noreply.github.com>
2025-09-17 00:52:50 +0530

    Update final.py

--- final.py
+++ final.py
@@ -180,7 +180,7 @@ if st.button("🚀 Run Extraction"):
             st.error("Excel must have a column named 'email'")
         else:
             st.info("🔄 Logging in to portal...")
-            session = login_to_portal(username, password)
+            session = login_to_portal(USERNAME,PASSWORD)
             if not session:
                 st.error("❌ Login failed. Check credentials.")
             else:
@@ -211,4 +211,5 @@ if st.button("🚀 Run Extraction"):
                     file_name=file_name,
                     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
                 )
+
 
